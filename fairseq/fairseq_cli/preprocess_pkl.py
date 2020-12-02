e539891eff392d388be1209977793ca27b687488
Eli <epugh@stanford.edu>
2020-12-02 03:51:03 -0500

    dict saving

--- fairseq/fairseq_cli/preprocess_pkl.py
+++ fairseq/fairseq_cli/preprocess_pkl.py
@@ -99,7 +99,7 @@ def main(args):
 
     corpus = torch.load(args.corpus)
     src_dict = build_dictionary(corpus)
-    src_dict.save(args.dir + "dict.txt")
+    src_dict.save(os.path.join(args.dir, "dict.txt"))
 
     def make_binary_dataset(vocab, input_prefix, output_prefix, lang, num_workers):
         logger.info("[{}] Dictionary: {} types".format(lang, len(vocab)))
